33ef2b813b5b0805ab01ae68d874a185297547e6
shannonstevens73 <shannon.stevens@bryanuniversity.edu>
2020-07-11 15:37:14 -0500

    submit second commit

--- week6/workbook/submit/app.js
+++ week6/workbook/submit/app.js
@@ -3,13 +3,20 @@
 // remember to pass "e" into the callback function
 
 var form = document.getElementById("submit-me")
+form.user_name.placeholder = "Name"
+form.age.placeholder = "Age"
 
 form.addEventListener("submit", function(e) {
     e.preventDefault()
+    
     alert(
             `
             Name: ${form.user_name.value}
             Age: ${form.age.value}`)
+
+            form.user_name.value = ""
+            form.age.value = ""
+
 
 
 })
